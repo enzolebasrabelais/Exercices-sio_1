97ae1cd0d31a13c09154637c3f7603e5ac407f7e
unknown <lebas@220056slocal>
2024-09-30 17:14:23 +0200

    Exercice_9_3 1

--- Exercice_9_1.vb
+++ Exercice_9_1.vb
@@ -1,21 +1,27 @@
 ﻿Module Exercice_9_1
     Sub Main()
-        Dim note, cpt, cpt2, pourcentage As Double
+        Dim note, cpt, cpt2, pourcentage, somme As Double
         cpt = 0
+        somme = 0
         Console.WriteLine("Entrez une note (-1 pour fin) :")
         note = Console.ReadLine()
         While note <> -1
             If note > 10 Then
                 cpt += 1
                 cpt2 += 1
+                somme += note
                 Console.WriteLine("Entrez une note (-1 pour fin) :")
                 note = Console.ReadLine()
             Else
                 cpt2 += 1
                 Console.WriteLine("Entrez une note (-1 pour fin) :")
                 note = Console.ReadLine()
+                If note <> -1 Then
+                    somme += note
+                End If
             End If
         End While
+        pourcentage = somme / cpt
 
         Console.WriteLine("Vous avez " + (pourcentage.ToString) + " de notes > à 10")
 
---
+++ Exercice_9_3.vb
@@ -0,0 +1,17 @@
+﻿Module Exercice_9_3
+    Sub Main()
+        Dim n, i, nombrePairs As Double
+        Do
+            Console.WriteLine("Entrez un nombre > à 0")
+            n = Console.ReadLine()
+        Loop Until n > 0
+        For i = 0 To n
+            nombrePairs += 2
+            If nombrePairs < n Or nombrePairs = n Then
+                Console.WriteLine("Les nombres pairs jusqu'à " + n.ToString() + ":" + nombrePairs.ToString())
+            End If
+
+        Next
+
+    End Sub
+End Module
