c23b979f4a6c2ea020a9b50e07c4a688ed822832
unknown <lebas@220056slocal>
2024-10-03 15:14:22 +0200

    début Exercice 9.4

--- Exercice_9_3.vb
+++ Exercice_9_3.vb
@@ -5,11 +5,11 @@
             Console.WriteLine("Entrez un nombre > à 0")
             n = Console.ReadLine()
         Loop Until n > 0
+        Console.WriteLine("Les " + n.ToString() + " premiers nombres pairs sont :")
         For i = 0 To n
+
             nombrePairs += 2
-            If nombrePairs < n Or nombrePairs = n Then
-                Console.WriteLine("Les nombres pairs jusqu'à " + n.ToString() + ":" + nombrePairs.ToString())
-            End If
+            Console.WriteLine(nombrePairs.ToString())
 
         Next
 
---
+++ Exercice_9_4.vb
@@ -0,0 +1,18 @@
+﻿Module Exercice_9_4
+    Sub Main()
+        Dim representant As String, numeroSemaine, ventes, total, commission As Double
+        For i = 0 To 4
+            Console.WriteLine("Nom du représentant ?")
+            representant = Console.ReadLine()
+            numeroSemaine += 1
+            Do
+                Console.WriteLine("Semaine " + numeroSemaine.ToString() + ": Ventes H.T hebdomadaires (0 pour stopper la saisie) ?")
+                ventes = Console.ReadLine()
+            Loop Until ventes > 0
+            total += ventes
+            Console.WriteLine("Bilan pour " + representant)
+            Console.WriteLine("Total des ventes H.T = " + total.ToString())
+        Next
+    End Sub
+
+End Module
