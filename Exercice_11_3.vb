eb606f622e3c4fda7a53642ca5d790518a02ee08
enzolebasrabelais <enzolebas190106@gmail.com>
2024-10-14 17:15:17 +0200

    Début Exercice_11_3

---
+++ Exercice_11_3.vb
@@ -0,0 +1,14 @@
+﻿Module Exercice_11_3
+    Const c = 300000
+    Function DilatationTemps(ByVal vfusee As Double, ByVal dfusee As Double) As Double
+        Return ((dfusee) / (Math.Sqrt(1 - ((vfusee ^ 2) / (c ^ 2)))))
+    End Function
+
+    Function ContractionLongueurs(ByVal vfusee As Double, ByVal tfusee As Double) As Double
+        Return ((tfusee) * (Math.Sqrt(1 - ((vfusee ^ 2) / (c ^ 2)))))
+    End Function
+
+    Function LoiComposition(ByVal vfusee As Double, ByVal vobus As Double) As Double
+        Return ((vobus + vfusee(())))
+    End Function
+End Module
